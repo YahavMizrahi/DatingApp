2f8bb4cc4fd371fb92cffeed9e6dd47e635eb669
YahavMizrahi <yahav283@gmail.com>
2020-11-02 15:45:00 +0200

    edit profile component, spinner component,can deactivate prevent save changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,9 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { SharedModule } from './_modules/shared.module';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -37,18 +40,21 @@ import { SharedModule } from './_modules/shared.module';
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent],
+    MemberCardComponent,
+    MemberEditComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi:true},
-    {provide:HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi:true}
+    {provide:HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi:true},
+    {provide:HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true}
 
   ],
   bootstrap: [AppComponent],
