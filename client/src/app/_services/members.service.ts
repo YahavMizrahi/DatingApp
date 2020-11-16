004e37f87392602e73081c015bb06f7a6035169f
YahavMizrahi <yahav283@gmail.com>
2020-11-16 20:01:22 +0200

    set main photo, delete photo

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -5,40 +5,45 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
-  baseurl=environment.apiUrl;
-  members:Member[]=[];
+  baseurl = environment.apiUrl;
+  members: Member[] = [];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getMembers(){
-    if(this.members.length>0)return of(this.members);
-    return this.http.get<Member[]>(this.baseurl+'users').pipe(
-      map(members=>{
-        this.members=members;
+  getMembers() {
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseurl + 'users').pipe(
+      map((members) => {
+        this.members = members;
         return members;
       })
     );
   }
 
-  getMember(username:string){
-    const member=this.members.find(x=>x.username===username)
-    if(member!==undefined) return of(member);
-    return this.http.get<Member>(this.baseurl+'users/'+username);
+  getMember(username: string) {
+    const member = this.members.find((x) => x.username === username);
+    if (member !== undefined) return of(member);
+    return this.http.get<Member>(this.baseurl + 'users/' + username);
   }
 
-  updateMember(member:Member){
-    return this.http.put(this.baseurl+'users',member).pipe(
-      map(()=>{
-        const index=this.members.indexOf(member);
-        this.members[index]=member;
+  updateMember(member: Member) {
+    return this.http.put(this.baseurl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
       })
-    )
+    );
+  }
 
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseurl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseurl + 'users/delete-photo/' + photoId, {});
   }
 }
