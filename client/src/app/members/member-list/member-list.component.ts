e2e15dfe2e35d1a9d084a2358708269ffcb3e0c9
YahavMizrahi <yahav283@gmail.com>
2021-01-02 13:07:08 +0200

    pagination users

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Pagination } from './../../_models/paginations';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
@@ -6,17 +7,31 @@ import { MembersService } from 'src/app/_services/members.service';
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$:Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor( private membersService:MembersService) { }
+  constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.members$=this.membersService.getMembers();
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.membersService
+      .getMembers(this.pageNumber, this.pageSize)
+      .subscribe((response) => {
+        this.members = response.result;
+        this.pagination = response.pagination;
+      });
+  }
 
-
+  pageChanged(event:     any) {
+    this.pageNumber = event.page;
+    this.loadMembers();
+  }
 }
