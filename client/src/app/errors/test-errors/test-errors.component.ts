b7decfe7eaaa8544579ca6b8b558964f4d3cc80d
YahavMizrahi <yahav283@gmail.com>
2021-02-12 16:06:42 +0200

    pre switch to postgres

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,60 +1,74 @@
+import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-errors',
   templateUrl: './test-errors.component.html',
-  styleUrls: ['./test-errors.component.css']
+  styleUrls: ['./test-errors.component.css'],
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl='https://localhost:5001/api/';
-  validationErrors:string[]=[];
+  // baseUrl='https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
+  validationErrors: string[] = [];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get404Err() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  get404Err(){
-    this.http.get(this.baseUrl+'buggy/not-found').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get400Err() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-    get400Err(){
-    this.http.get(this.baseUrl+'buggy/bad-request').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get500Err() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-    get500Err(){
-    this.http.get(this.baseUrl+'buggy/server-error').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get401Err() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-    get401Err(){
-    this.http.get(this.baseUrl+'buggy/auth').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get400ValidationErr() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      }
+    );
   }
-
-      get400ValidationErr(){
-    this.http.post(this.baseUrl+'account/register',{}).subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-      this.validationErrors=error;
-    })
-  }
-  
-
 }
