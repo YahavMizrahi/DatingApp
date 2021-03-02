c88fd21935d792785a4c0381ce8c0d34dc159210
YahavMizrahi <yahav283@gmail.com>
2021-03-02 19:15:21 +0200

    fix msg

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,5 +1,5 @@
+import { BusyService } from './busy.service';
 import { Group } from './../_models/group';
-import { error } from 'protractor';
 import { take } from 'rxjs/operators';
 import { User } from './../_models/user';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
@@ -20,9 +20,10 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private busyService: BusyService) {}
 
   createHubConnection(user: User, otherUserName: String) {
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.huburl + 'message?user=' + otherUserName, {
         accessTokenFactory: () => user.token,
@@ -30,7 +31,11 @@ export class MessageService {
       .withAutomaticReconnect()
       .build();
 
-    this.hubConnection.start().catch((error) => console.log(error));
+    this.hubConnection
+      
+      .start()
+      .catch((error) => console.log(error))
+      .finally(()  =>  this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', (messages) => {
       this.messageThreadSource.next(messages);
@@ -42,22 +47,22 @@ export class MessageService {
     });
 
     this.hubConnection.on('UpdatedGroup', (group: Group) => {
-      if (group.connection.some(x => x.username === otherUserName)) {
-        this.messageThread$.pipe(take(1)).subscribe(messages => {
-          messages.forEach(message => {
-            
+      if (group.connection.some((x) => x.username === otherUserName)) {
+        this.messageThread$.pipe(take(1)).subscribe((messages) => {
+          messages.forEach((message) => {
             if (!message.dateRead) {
               message.dateRead = new Date(Date.now());
             }
           });
           this.messageThreadSource.next([...messages]);
-        })
+        });
       }
     });
   }
 
   stopHubConnection() {
     if (this.stopHubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
   }
@@ -79,11 +84,13 @@ export class MessageService {
     );
   }
 
-   async sendMessage(username: string, content: string) {
-    return this.hubConnection.invoke('SendMessage', {
-      recipientUsername: username,
-      content,
-    }).catch(error=>console.log(error));
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection
+      .invoke('SendMessage', {
+        recipientUsername: username,
+        content,
+      })
+      .catch((error) => console.log(error));
   }
 
   deleteMessage(id: number) {
